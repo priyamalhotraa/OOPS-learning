5280629fbb0783c0cb20cfc10ce76c779bedcf71
priyamalhotraa <priyamalhotra14012005@gmail.com>
2026-06-21 23:25:51 +0530

    Extensibility

--- Inheritance.cpp
+++ Inheritance.cpp
@@ -43,10 +43,11 @@ public:
 
 class Car : public Vehicle //child class
 {
-    public:
+protected:
     int noOfDoors;
     string transmissionType;
 
+public:
     Car(string _name, string _model, int _noOfTyres, int _noOfDoors, string _transmissionType):Vehicle(_name, _model, _noOfTyres)
     {
         cout << "I am inside Car constructor " << endl;
@@ -60,11 +61,35 @@ class Car : public Vehicle //child class
     }
 };
 
+class MotorCycle : public Vehicle
+{
+protected:
+    string handleBarStyle;
+    string suspensionType;
+
+public:
+    MotorCycle(string _name, string _model, int _noOfTyres, string _handleBarStyle, string _suspensionType):Vehicle(_name, _model, _noOfTyres)
+    {
+        this->handleBarStyle = _handleBarStyle;
+        this->suspensionType = _suspensionType;
+    }
+
+    void wheelie()
+    {
+        cout << "Performing wheelie " << endl;
+    }
+};
+
 int main()
 {
     Car A("Maruti 800", "LXI", 4, 4, "Manual");
     A.start_engine();
     A.startAC();
     A.stop_engine();
+
+    MotorCycle M("BMW", "VXI", 2, "U", "Hard");
+    M.start_engine();
+    M.wheelie();
+    M.stop_engine();
     return 0;
 }
