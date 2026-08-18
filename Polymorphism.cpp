c1d04d67d0849edc5eabb019b1ad1caa1aaf8ef5
priyamalhotraa <priyamalhotra14012005@gmail.com>
2026-08-18 18:20:56 +0530

    compile time polymorphism

--- Polymorphism.cpp
+++ Polymorphism.cpp
@@ -10,18 +10,21 @@ class Add {
     // x, y, two int addition
     int sum(int x, int y)
     {
+        cout << "Sum of 2 int" << endl;
         return x + y;
     }
 
     // x, y, z, three int add
     int sum(int x, int y, int z)
     {
+        cout << "Sum of 3 int" << endl;
         return x + y + z;
     }
 
     // double add
     double sum(double x, double y)
     {
+        cout << "Sum of 2 doubles" << endl;
         return x + y;
     }
 
@@ -33,6 +36,9 @@ int main()
     int z = 2;
 
     Add add;
-    cout << add.sum(x, y)
+    cout << add.sum(x, y) << endl;
+    cout << add.sum(x, y,z) << endl;
+
+    cout << add.sum(5.4, 2.3) << endl;
     return 0;
 }
